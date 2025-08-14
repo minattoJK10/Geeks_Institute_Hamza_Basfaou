908cb087b754aa3235ffb3ea3f23a859db065bad
minattoJK10 <hamza2000basf@gmail.com>
2025-08-14 12:25:03 +0100

    Day3

--- week1/day3/exercices/exercice1.py
+++ week1/day3/exercices/exercice1.py
@@ -7,16 +7,11 @@ class Cat:
 def find_oldest_cat(cats):
     return max(cats, key=lambda cat: cat.age)
 
-
 cat1 = Cat("mimi", 3)
 cat2 = Cat("Buu", 4)
 cat3 = Cat("Shadow", 5)
-
-# Put them into a list
 cats = [cat1, cat2, cat3]
 
-# Find the oldest cat
 oldest = find_oldest_cat(cats)
 
-# Print result
 print(f"The oldest cat is {oldest.name}, and is {oldest.age} years old.")
