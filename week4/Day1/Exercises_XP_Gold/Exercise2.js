84d334fbca00573c569c40e0dce475f46ae1a1d7
minattoJK10 <hamza2000basf@gmail.com>
2025-09-08 13:50:02 +0100

    Week4 Day1 Exercises_XP_Gold Mis A jour

--- week4/Day1/Exercises_XP_Gold/Exercise2.js
+++ week4/Day1/Exercises_XP_Gold/Exercise2.js
@@ -1,3 +1,11 @@
+// Analyse: 
+// addTo is a function that returns another function.
+// x is "remembered" inside the inner function y => x + y due to closure.
+// addToTen stores the inner function with x = 10.
+// Calling addToTen(3) adds 10 + 3.
 const addTo = x => y => x + y;
-const addToTen = addTo(10);
-console.log(addToTen(3));
+
+const addToTen = addTo(10); // returns y => 10 + y
+
+// Prediction: 10 + 3 = 13
+console.log(addToTen(3)); // 13
--- week4/Day1/Exercises_XP_Gold/Exercise5.js
+++ week4/Day1/Exercises_XP_Gold/Exercise5.js
@@ -1,4 +1,11 @@
+// Analyse:
+// compose(f, g) returns a function a => f(g(a))
+// add5(10) = 15
+// add1(15) = 16
+// So compose(add1, add5)(10) = add1(add5(10)) = 16
 const compose = (f, g) => (a) => f(g(a));
 const add1 = (num) => num + 1;
 const add5 = (num) => num + 5;
-console.log(compose(add1, add5)(10));
+
+// Prediction: 16
+console.log(compose(add1, add5)(10)); // 16
