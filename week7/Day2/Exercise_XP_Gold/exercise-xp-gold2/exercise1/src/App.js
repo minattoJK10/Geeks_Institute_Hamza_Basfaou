6330b6fa19b56fba9aca77224fa866b5b9103ed2
minattoJK10 <hamza2000basf@gmail.com>
2025-10-13 11:06:56 +0100

    week7 Day2 Exercise_XP_Gold#2

---
+++ week7/Day2/Exercise_XP_Gold/exercise-xp-gold2/exercise1/src/App.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import FormSubmit from './components/FormSubmit';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+      <FormSubmit />
+    </div>
+  );
+}
+
+export default App;
